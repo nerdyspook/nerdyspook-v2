20e22e28b95580b66cffa0010735a0040147912b
Susanto <nerdyspook007@gmail.com>
2022-07-15 20:59:29 +0530

    feat: added index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
-const Page = () => {
-  return <div>Hello</div>
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" mb="6" p="3" align="center">
+        Hello, I&apos;m a full-stack developer based in India!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            NerdySpook
+          </Heading>
+          <p>Digital Craftzman ( Artist / Developer / Designer )</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
-export default Page
+export default Home
