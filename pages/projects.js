128dbfa4052676b687dba900c53af0cd05361f91
Susanto <nerdyspook007@gmail.com>
2022-07-17 10:04:05 +0530

    feat: added few projects in projects page

---
+++ pages/projects.js
@@ -0,0 +1,33 @@
+import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumbVolt from '../public/images/volt-home.jpg'
+import thumbFrontendPrep from '../public/images/prep-home.jpg'
+
+const Projects = () => {
+  return (
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Projects
+      </Heading>
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id="ecommerce" title="Volt" thumbnail={thumbVolt}>
+            An Ecommerce app for shoes.
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem
+            id="frontend-prep"
+            title="Frontend-Prep"
+            thumbnail={thumbFrontendPrep}
+          >
+            A quiz app to test your knowledge in frontend technologies.
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+    </Container>
+  )
+}
+
+export default Projects
