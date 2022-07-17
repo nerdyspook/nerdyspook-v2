82e22a8d1edda99dbe88a4359201ee8b6f06b2c0
Susanto <nerdyspook007@gmail.com>
2022-07-18 00:27:13 +0530

    feat: added error page

---
+++ pages/404.js
@@ -0,0 +1,28 @@
+import NextLink from 'next/link'
+import {
+  Box,
+  Heading,
+  Text,
+  Container,
+  Divider,
+  Button
+} from '@chakra-ui/react'
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Heading as="h1" mb={4}>
+        404: Page Not Found
+      </Heading>
+      <Text>🐶 Woofy couldn&apos;t find the page you were looking for.</Text>
+      <Divider my={6} />
+      <Box my={6} align="center">
+        <NextLink href="/">
+          <Button colorScheme="teal">Return to home</Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
+}
+
+export default NotFound
