a6960031c67ebe3bc029b1fd58d3e32e63c93815
Susanto <nerdyspook007@gmail.com>
2022-09-06 17:50:31 +0530

    remove grid-item

--- pages/blogs.js
+++ pages/blogs.js
@@ -14,11 +14,35 @@ const Blogs = () => (
       </Heading>
       <Section delay={0.1}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
+          {/* <GridItem
             title="Semantic Git Commit messages which everyone should follow!"
             thumbnail={'/images/blogTwo.jpg'}
             href="https://nerdyspook.hashnode.dev/semantic-git-commit-messages-which-everyone-should-follow"
-          />
+          /> */}
+
+          <Box w="100%" align="center">
+            <LinkBox cursor="pointer">
+              <Image
+                src="/images/blogTwo.jpg"
+                alt="Semantic Git Commit messages which everyone should follow!"
+                className="grid-item-thumbnail"
+                // placeholder="blur"
+                // layout="fill"
+                height={250}
+                width={500}
+                loading="lazy"
+              />
+              <LinkOverlay href={href} target="_blank">
+                <Text mt={2}>
+                  Semantic Git Commit messages which everyone should follow!
+                </Text>
+              </LinkOverlay>
+              <Text fontSize={14}>
+                Semantic Git Commit messages which everyone should follow!
+              </Text>
+            </LinkBox>
+          </Box>
+
           <GridItem
             title="CSS Pseudo-elements"
             thumbnail={'/images/blogOne.jpg'}
